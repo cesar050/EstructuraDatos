531cd5c79484201b982f17c211bf77b44d552ef0
César Daniel Ramos Merchán <cesar.d.ramos@unl.edu.ec>
2024-06-17 23:02:17 -0500

    Grafo

---
+++ Estructuras/src/Grafo.java
@@ -0,0 +1,4 @@
+public abstract class Grafo extends EstructuraDato {
+
+
+}
---
+++ Estructuras/src/NoDirigido.java
@@ -0,0 +1,16 @@
+public class NoDirigido extends Grafo{
+    @Override
+    public void agregar(int dato) {
+
+    }
+
+    @Override
+    public void insertar(int dato) {
+
+    }
+
+    @Override
+    public void eliminar(int dato) {
+
+    }
+}
